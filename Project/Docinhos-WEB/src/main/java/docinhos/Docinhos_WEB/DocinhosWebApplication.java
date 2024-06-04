03888e5f87f0abc420e94ae622ba1b99c38615f2
HevertSousa <hevert.sousa@outlook.com>
2024-06-04 15:58:53 -0300

    novo projeto

---
+++ Project/Docinhos-WEB/src/main/java/docinhos/Docinhos_WEB/DocinhosWebApplication.java
@@ -0,0 +1,13 @@
+package docinhos.Docinhos_WEB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocinhosWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocinhosWebApplication.class, args);
+	}
+
+}
---
+++ Project/Docinhos-WEB/src/test/java/docinhos/Docinhos_WEB/DocinhosWebApplicationTests.java
@@ -0,0 +1,13 @@
+package docinhos.Docinhos_WEB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocinhosWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
